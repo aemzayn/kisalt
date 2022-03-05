132c031f7fca279fac60691bbeb1d9e63b4c061e
aemzayn <ahmad.muslih22@gmail.com>
2022-03-05 20:33:17 +0300

    finish delete url api

--- context/AlertContext.tsx
+++ context/AlertContext.tsx
@@ -55,6 +55,7 @@ export const AlertProvider = ({ children }) => {
       onConfirm: args.onConfirm || closeAlert,
     })
     openCover()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
