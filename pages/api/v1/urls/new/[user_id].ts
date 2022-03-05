70016d84d90e7b0c9dd0e74299b6ff3251268527
aemzayn <ahmad.muslih22@gmail.com>
2022-03-05 21:43:15 +0300

    add alert when fail to create new url and mutate when success

--- pages/api/v1/urls/new/[user_id].ts
+++ pages/api/v1/urls/new/[user_id].ts
@@ -22,8 +22,6 @@ export default async function handler(
       return
     }
 
-    // check if slug already being used
-
     const { data: slugIsUsed } = await supabase
       .from('urls')
       .select('slug')
